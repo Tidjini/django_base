0b9eeb5e7a34f24926ce212bc04cb569666b9568
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-19 16:07:37 +0100

    set debug variable in different context dev/test/prod settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,8 +32,6 @@ def get_environ_variable(var_name):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_environ_variable('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
