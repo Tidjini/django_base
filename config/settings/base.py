56057be697487b6536eba1674c916d72623391b7
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-19 15:58:34 +0100

    set default database wth dj_database_url with decouple

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,10 +91,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
