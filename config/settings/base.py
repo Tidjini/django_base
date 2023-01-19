12d4a63042881ce4837c8c50001ce01961811388
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-19 16:02:36 +0100

    set drf configurations

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,3 +136,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DRF Base configurations
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 200,
+    "COERCE_DECIMAL_TO_STRING": False,
+    "COERCE_DATE_TO_STRING": False,
+}
