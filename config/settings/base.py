fa04966178afb4b107a9ef3b6c51d87d015fb246
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-19 15:54:28 +0100

    add third party libraries in INSTALLED APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party library
+    "django_filters",
+    "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken",
+
 ]
 
 MIDDLEWARE = [
